31ee4da394225e38d316e6d9157597d05de45582
Cynthia <xcynthia@live.unc.edu>
2019-11-01 18:54:06 -0400

    updated winCheck fxn

--- learn.py
+++ learn.py
@@ -1,7 +1,9 @@
 import sys
+import collections
 
 runs = sys.argv[1]
 board = [[[0]*4]*4]*4
+# board[row][col][floor]
 
 potential_moves = {}
 moves = {}
@@ -13,26 +15,47 @@ for i in range(4):
             potential_moves[coord] = 0
 
 
-def winCheck(move, player, count):
+def winCheck(move, player):
+    # 0 is no win, 1 is yes win
+
     row, col, floor = move[0], move[1], move[2]
-    if -1 in {row, col, floor} or 4 in {row, col, floor}:
-        return count
-    if board[row][col][floor] == player:
-        count = count+1
-    winCheck([row + 1, col, floor], player, count + 1)
-    winCheck([row - 1, col, floor], player, count + 1)
-    winCheck([row, col + 1, floor], player, count + 1)
-    winCheck([row, col - 1, floor], player, count + 1)
-    winCheck([row, col, floor + 1], player, count + 1)
-    winCheck([row, col, floor - 1], player, count + 1)
+
+    r = collections.Counter([board[i][col][floor] for i in range(4)])
+    if r[player] == 4:
+        return 1
+    c = collections.Counter([board[row][i][floor] for i in range(4)])
+    if c[player] == 4:
+        return 1
+    f = collections.Counter([board[row][col][i] for i in range(4)])
+    if f[player] == 4:
+        return 1
+
+    # check diagonals
     if row / col == 1:
-        winCheck([row + 1, col + 1, floor], player, count + 1)
-        winCheck([row - 1, col - 1, floor], player, count + 1)
+        d0 = collections.Counter([board[i][i][floor] for i in range(4)])
+    elif row + col == 3:
+        d0 = collections.Counter([board[i][3-i][floor] for i in range(4)])
+    if d0[player] == 4:
+        return 1
+
     if col / floor == 1:
-        winCheck([row, col + 1, floor + 1], player, count + 1)
-        winCheck([row, col - 1, floor - 1], player, count + 1)
-    if row / floor == 1:
-        winCheck([row + 1, col, floor + 1], player, count + 1)
-        winCheck([row - 1, col, floor - 1], player, count + 1)
+        d1 = collections.Counter([board[row][i][i] for i in range(4)])
+    elif col + floor == 3:
+        d1 = collections.Counter([board[row][i][3-i] for i in range(4)])
+    if d1[player] == 4:
+        return 1
+
+    if floor / row == 1:
+        d2 = collections.Counter([board[i][col][i] for i in range(4)])
+    elif floor + row == 3:
+        d2 = collections.Counter([board[i][col][3-i] for i in range(4)])
+    if d2[player] == 4:
+        return 1
+
+    return 0
+
+
+
+
 
 
