9030f33083787a9c287367ecdf31a75b4ce30d65
Cynthia <xcynthia@live.unc.edu>
2019-11-01 22:52:00 -0400

    fixed div by 0 error

--- learn.py
+++ learn.py
@@ -7,13 +7,13 @@ board = [[[0] * 4] * 4] * 4
 
 potential_moves = {}
 moves = {}
-"""
-# for i in range(4):
+
+for i in range(4):
     for j in range(4):
         for k in range(4):
-            coord = [i, j, k]
+            coord = (i, j, k)
             potential_moves[coord] = 0
-"""
+
 def printBoard():
     for x in range(len(board)):
         print(board[x])
@@ -35,21 +35,21 @@ def winCheck(move, player):
         return 1
 
     # check diagonals
-    if row / col == 1:
+    if row - col == 0:
         d0 = collections.Counter([board[i][i][floor] for i in range(4)])
     elif row + col == 3:
         d0 = collections.Counter([board[i][3 - i][floor] for i in range(4)])
     if d0[player] == 4:
         return 1
 
-    if col / floor == 1:
+    if col - floor == 0:
         d1 = collections.Counter([board[row][i][i] for i in range(4)])
     elif col + floor == 3:
         d1 = collections.Counter([board[row][i][3 - i] for i in range(4)])
     if d1[player] == 4:
         return 1
 
-    if floor / row == 1:
+    if floor - row == 0:
         d2 = collections.Counter([board[i][col][i] for i in range(4)])
     elif floor + row == 3:
         d2 = collections.Counter([board[i][col][3 - i] for i in range(4)])
